b5a8be87007abf0d8118316060642cae8efe283e
lechemin1775 <lechemin1775@gmail.com>
2026-07-19 15:15:52 +0200

    sondage_bot.js

--- sondage_bot.js
+++ sondage_bot.js
@@ -67,7 +67,7 @@ async function executerRituelQuotidien() {
         const footerHTML = `\n\n🌐 <a href="${urlSite}">Le Portail de Lumière</a>\n📱 <a href="${urlApp}">Application Cobrapédia pour Android</a>`;
 
         // 4. Détermination du moment (Trois marches distinctes)
-        const heureActuelleParis = new Date().getUTCHours() + 2; 
+        const heureActuelleParis = 10; 
         
         if (heureActuelleParis < 9) {
             // ==========================================
